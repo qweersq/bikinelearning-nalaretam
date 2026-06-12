da0a6ab2a68cf1efc8cf601072fe8e972be1276f
qweersq <akbarhadadal@gmail.com>
2026-06-12 11:29:54 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: "nalaretam",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3003",
+      args: "start -p 3004",
       instances: "max", // Runs in cluster mode using all available CPU cores
       exec_mode: "cluster",
       watch: false,
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        PORT: 3003
+        PORT: 3004
       }
     }
   ]
